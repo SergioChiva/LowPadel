55ef4e07ffc4a03f43821c107ad6783c0a583b7d
Sergio Chiva <sergiochiva2002@gmail.com>
2025-12-06 15:20:55 +0100

    Fix: Añadir is:inline a scripts y corregir config

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,5 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
-import vercel from '@astrojs/vercel/static';
 
 // https://astro.build/config
-export default defineConfig({
-  output: 'static',
-  adapter: vercel()
-});
+export default defineConfig({});
